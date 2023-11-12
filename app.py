d367b538624883753eb31a7c692a0f548f744804
Thomas <123456.max.thomas@gmail.com>
2023-11-12 02:07:01 -0500

    refactored code
    Integrated links for each page into the template and made very basic functionality for each in app.py. renamed the app folder as it was causing directory issues when linking to pages

--- app.py
+++ app.py
@@ -1,6 +1,48 @@
-from app import create_app
+from flask import Flask, render_template, request, redirect, abort
 
-app = create_app()
+app = Flask(__name__, root_path='content/')
 
-if __name__ == '__main__':
-    app.run()
+#HOME PAGE
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#COMMUNITY PAGE
+@app.route('/community')
+def community():
+    return render_template('community.html')
+
+#CONTACT PAGE
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+#CREATE LISTING PAGE
+@app.route('/create_listing')
+def create_listing():
+    return render_template('create_listing.html')
+
+#LOGIN PAGE
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+#MARKETPLACE PAGE
+@app.route('/marketplace')
+def marketplace():
+    return render_template('marketplace.html')
+
+#PROFILE PAGE
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+#SETTINGS PAGE
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+#USER PAGE
+@app.route('/user')
+def user():
+    return render_template('user.html')
