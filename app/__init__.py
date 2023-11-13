5c399bd3b32cef4715c97c3120465ab3b63956b7
Riley_Retzloff <rirye811@gmail.com>
2023-11-12 19:15:27 -0500

    Added first draft of template.html and main.css. Both still need significant work.

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ def create_app():
     app = Flask(__name__)
 
     # from .routes import route_1, route_2, ...
+    from .routes import index_routes
 
-    # app.register_blueprint(route_1)
+    # app.register_blueprint(page_routes.blueprint_name)
     # app.register_blueprint(...)
+    app.register_blueprint(index_routes.index)
 
     return app
