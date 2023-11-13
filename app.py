506cd06ab9acaa31f554844cb2b11c47052fbdea
Mekhi <mekhi.j.glover@gmail.com>
2023-11-13 18:04:58 -0500

    basic changes

--- app.py
+++ app.py
@@ -10,3 +10,12 @@ def index():
 @app.route('/profile', methods=['POST','GET'])
 def profile():
     return render_template('profile.html')
+
+
+
+@app.post('/login')
+def sign_up(email,password):
+    # if the email matches the password given
+    # redirect to the user page
+    # else, throw an error or refresh the page to try again
+    return redirect('profile/<')
