2fe7885f6c9100e03a3020f44110f91105a61efc
Mekhi <mekhi.j.glover@gmail.com>
2023-11-15 18:38:13 -0500

    Basics of the login page

--- app.py
+++ app.py
@@ -3,19 +3,23 @@ from flask import Flask,render_template, request,redirect,abort,url_for
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-@app.route('/profile', methods=['POST','GET'])
-def profile():
-    return render_template('profile.html')
+# @app.route('/profile', methods=['POST','GET'])
+# def profile():
+#     return render_template('profile.html')
+@app.get('/')
+def display():
+    return render_template('login.html')
 
 
-
-@app.post('/login')
-def sign_up(email,password):
+@app.get('/login')
+def sign_up():
     # if the email matches the password given
+    email = request.form.get('email')
     # redirect to the user page
     # else, throw an error or refresh the page to try again
-    return redirect('profile/<')
+    print(f'Hello {email}')
+    return render_template('login.html')
