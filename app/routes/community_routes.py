79ae325e89495fd8be59aab7ba1e1416ff4ba90a
Avery <averynewkirk@gmail.com>
2023-11-20 13:26:35 -0500

    Community page update

--- app/routes/community_routes.py
+++ app/routes/community_routes.py
@@ -1,7 +1,23 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+import random
 
 community = Blueprint('community', __name__)
 
 @community.route('/community')
 def community_page():
-    return render_template('community.html')
+
+
+    """
+    TODO
+    Fix template elemetns so that the webpage should 
+    """
+
+    """
+    community page must always be a multiple of 3 
+    to get the correct "grid" effect
+    """
+    num_pages = random.randint(1,6) * 3 
+    
+
+    
+    return render_template('community.html', num_pages=num_pages)
