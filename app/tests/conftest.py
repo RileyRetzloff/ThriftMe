117af7b731d7143259394a457428bb1ac42da5c1
AveryNewkirk <143564262+AveryNewkirk@users.noreply.github.com>
2023-12-12 21:25:47 -0500

    Added more tests and database clearing method. (#38)
    * added more tests and database clearing method
    
    * more tests and fixed password storage
    
    * Simulate file uploads and viewing community post in tests.
    
    * More tests and an extra client for better testing
    
    ---------
    
    Co-authored-by: Avery <averynewkirk@gmail.com>

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,24 +1,54 @@
 import os 
 import pytest
 
+
 from app import create_app
+from ..database import db
+from sqlalchemy import text
 
 
+#Flag application for testing and clear database before and after each test
 @pytest.fixture()
 def app(): 
     
-    #flag for testing
     os.environ['FLASK_ENV'] = 'testing'
     app = create_app()
-
+    clear_db()
     yield app
+    clear_db()
+   
 
 
 @pytest.fixture()
 def client(app):
     return app.test_client()
 
+#extra client to simulate multiple users
+@pytest.fixture()
+def client_2(app):
+    return app.test_client()
+
 
 @pytest.fixture()
 def runner(app):
     return app.test_cli.runner()
+
+
+#run SQL script to clear the database
+def clear_db():
+   
+    delete_command = """
+            select tablename from pg_tables where schemaname = 'test_thriftme';
+
+            DO $$
+            DECLARE
+                table_name text;
+            BEGIN
+                FOR table_name IN (SELECT tablename FROM pg_catalog.pg_tables where schemaname= 'public')
+                LOOP
+                    EXECUTE 'DELETE FROM ' || quote_ident(table_name) || ' CASCADE';
+                END LOOP;
+            END $$;
+                    """
+    db.session.execute(text(delete_command))
+    db.session.commit()
