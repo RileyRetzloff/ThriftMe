13ebb9587d595af3a1ca454516dbc4f8b1a2c072
Hayden <ginger52703@gmail.com>
2023-12-13 23:20:08 -0500

    added profile picture change and username on banner

--- app/routes/profile_routes.py
+++ app/routes/profile_routes.py
@@ -1,29 +1,44 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, redirect, session, send_file
 from ..models.pipeline import Users,db
-from ..utils import upload_file
-
 profile = Blueprint('main', __name__)
+import base64
 
 
 #################
 @profile.route('/profile', methods=['POST','GET'])
 def render_profile():
-    #return user to signup if user is not logged in i.e. username returns None
+    # Return user to signup if the user is not logged in, i.e., username returns None
     username = session.get('username')
-    
-    if username == None:
+
+    if username is None:
         print("No user is logged in.")
         return render_template('signup.html')
-    else:
-        print(f"User {username} is logged in.")
-        return render_template('profile.html', username=username)
+
+    user = Users.get_by_username(username)
+
+    if user is None:
+        print(f"User {username} not found.")
+        return render_template('signup.html')
+
+    profile_picture_base64 = None
+
+    if user.profile_picture is not None:
+        profile_picture_base64 = base64.b64encode(user.profile_picture).decode('utf-8')
+
+    print(f"User {username} is logged in.")
+    return render_template('profile.html', username=username, profile_picture=profile_picture_base64)
     
 ##################
 
 @profile.route('/edit_profile', methods=['POST','GET'])
 def render_profile_settings():
     username = session.get('username')
-    return render_template('profile_settings.html', username = username)
+    user = Users.get_by_username(username)
+    profile_picture_base64 = None
+    if user.profile_picture is not None:
+        profile_picture_base64 = base64.b64encode(user.profile_picture).decode('utf-8')
+
+    return render_template('profile_settings.html', username = username, profile_picture=profile_picture_base64)
 
 ##################
 
@@ -42,15 +57,32 @@ def render_edit_profile_user():
             session['username'] = newUsername
         return redirect('/edit_profile')  
 
-    return render_template('edit_username_form.html', username=username)  
+    return render_template('index.html', username=username)  
 
 ##################
 
-@profile.route('/edit_profile_pic', methods=['POST','GET'])
-def render_edit_profile_profile():
+@profile.route('/edit_profile_pic', methods=['POST', 'GET'])
+def render_edit_profile():
     username = session.get('username')
+    user = Users.get_by_username(username)
+    allowed_extensions = {'jpg', 'jpeg'}
+    profile_picture_base64 = None  # Default value
 
-    return render_template('profile_settings.html', username = username)
+    if request.method == 'POST' and 'image' in request.files:
+        new_pfp = request.files['image']
+        if '.' in new_pfp.filename and new_pfp.filename.rsplit('.', 1)[1].lower() in allowed_extensions:
+            # Update the user's profile picture in the database
+            user.profile_picture = new_pfp.read()
+            db.session.commit()
+
+            profile_picture_base64 = base64.b64encode(user.profile_picture).decode('utf-8')
+
+        else:
+            return render_template('profile_settings.html', username=username, profile_picture=profile_picture_base64)
+
+    return render_template('profile_settings.html', username=username, profile_picture=profile_picture_base64)
+
+##################
 
 @profile.route('/logout', methods=['POST','GET'])
 def logout():
