78cadf8bc2be91a959b92402ad56d5dd453af071
Avery <averynewkirk@gmail.com>
2023-12-14 15:21:41 -0500

    Hot fix to get the app running again.

--- app/routes/listing_card_test_routes.py
+++ app/routes/listing_card_test_routes.py
@@ -1,4 +1,4 @@
-from crypt import methods
+#from crypt import methods
 from flask import Blueprint, render_template
 from ..models.pipeline import Listing, Album, Photo
 
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,4 +1,4 @@
-from crypt import methods
+#from crypt import methods
 from flask import Blueprint, render_template, session,redirect
 from ..models.pipeline import Users
 user = Blueprint('user', __name__)
